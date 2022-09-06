71cabf1dd87f5a247557385aed2d0f919c6d7819
K Raj <kamarajKannan23@gmail.com>
2022-09-06 13:20:45 +0530

    report added

--- src/test/java/com/runner/TestRunnerClass.java
+++ src/test/java/com/runner/TestRunnerClass.java
@@ -1,6 +1,13 @@
 package com.runner;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
+
+import com.base.RestAssuredBaseClass;
+import com.reports.Reporting;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -9,8 +16,11 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 @RunWith(Cucumber.class)
 //tags = "@LoginAndaddAddress"
 @CucumberOptions( dryRun = false, features = "src\\test\\resources\\Features", glue = "com.stepdefinition", stepNotifications = true, publish = true, plugin = {
-		"pretty" }, monochrome = true, snippets = SnippetType.CAMELCASE)
+		"pretty","json:target\\output.json" }, monochrome = true, snippets = SnippetType.CAMELCASE)
 
-public class TestRunnerClass {
-
+public class TestRunnerClass extends RestAssuredBaseClass {
+	@AfterClass
+	public static void afterClass() throws FileNotFoundException, IOException {
+		Reporting.generateJVMReport(System.getProperty("user.dir")+getPropertyFileValue("jsonPath"));
+	}
 }
